40e86b18963354ae4f47a75fdd999f30c6230c78
gyunnybot <gyunnybot4029@gmail.com>
2025-09-10 19:23:33 +0900

    fix comment

--- membermanagement/src/main/java/member/membermanagement/MembermanagementApplication.java
+++ membermanagement/src/main/java/member/membermanagement/MembermanagementApplication.java
@@ -12,12 +12,12 @@ public class MembermanagementApplication {
 
     /**
      * h2 database 실행하기
-     * Windows : ~\H2\bin 폴더 경로 이동 -> h2.bat 실행s
+     * Windows : ~\H2\bin 폴더 경로 이동 -> h2.bat 실행
      * MacOS : ~/H2/bin 폴더 경로 이동 -> ./h2.sh 실행
      *
      * 처음 접속 시, 연결 후 전체 디렉토리에 test.mv.db 폴더가 있는지 확인
-     * 이후, jdbc:h2:tcp://localhost/~/test
+     * 이후 jdbc:h2:tcp://localhost/~/test
      *
-     * sql package 내 query 입력 후 실행
+     * 처음 접속 시, sql package 내 query 입력 후 실행
      * */
 }
