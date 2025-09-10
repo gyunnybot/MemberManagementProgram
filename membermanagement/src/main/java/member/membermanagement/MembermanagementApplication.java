da8ebde0b60f1b6059ac1e948fe71f76e46e3538
gyunnybot <gyunnybot4029@gmail.com>
2025-09-10 18:58:44 +0900

    add h2 database path in MacOS

--- membermanagement/src/main/java/member/membermanagement/MembermanagementApplication.java
+++ membermanagement/src/main/java/member/membermanagement/MembermanagementApplication.java
@@ -9,5 +9,15 @@ public class MembermanagementApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MembermanagementApplication.class, args);
 	}
-	// db 실행 : C:\Program Files (x86)\H2\bin -> h2.bat
+
+    /**
+     * h2 database 실행하기
+     * Windows : ~\H2\bin 폴더 경로 이동 -> h2.bat 실행s
+     * MacOS : ~/H2/bin 폴더 경로 이동 -> ./h2.sh 실행
+     *
+     * 처음 접속 시, 연결 후 전체 디렉토리에 test.mv.db 폴더가 있는지 확인
+     * 이후, jdbc:h2:tcp://localhost/~/test
+     *
+     * sql package 내 query 입력 후 실행
+     * */
 }
