d7fbf817d0f98f6194ec8aea4fe3cc0c028be956
gyunnybot <idjkda4034@gmail.com>
2025-09-12 09:53:22 +0900

    modified MembermanagementApplication comment

--- membermanagement/src/main/java/member/membermanagement/MembermanagementApplication.java
+++ membermanagement/src/main/java/member/membermanagement/MembermanagementApplication.java
@@ -11,13 +11,15 @@ public class MembermanagementApplication {
 	}
 
     /**
-     * h2 database 실행하기
+     * H2 database를 별도로 설치 후 실행해야 합니다!!
+	 * 
+	 * H2 다운로드 후, 저장 경로로 이동
+	 * 
      * Windows : ~\H2\bin 폴더 경로 이동 -> h2.bat 실행
      * MacOS : ~/H2/bin 폴더 경로 이동 -> ./h2.sh 실행
      *
-     * 처음 접속 시, 연결 후 전체 디렉토리에 test.mv.db 폴더가 있는지 확인
-     * 이후 jdbc:h2:tcp://localhost/~/test
-     *
-     * 처음 접속 시, sql package 내 query 입력 후 실행
-     * */
+     * 처음 접속 시, 연결 후 전체 디렉토리에 test.mv.db 폴더가 생성되었는지 확인 + sql package 내 query 입력하여 테이블 생성
+	 *
+     * 이후 jdbc:h2:tcp://localhost/~/test 경로 확인 후 접속 가능
+	 */
 }
