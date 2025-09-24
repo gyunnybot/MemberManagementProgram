e81ac4881e542982087a95a0ccb5ceb1cbc7add6
gyunnybot <idjkda4034@gmail.com>
2025-09-24 10:05:44 +0900

    Update MembermanagementApplication.java

--- membermanagement/src/main/java/member/membermanagement/MembermanagementApplication.java
+++ membermanagement/src/main/java/member/membermanagement/MembermanagementApplication.java
@@ -11,15 +11,16 @@ public class MembermanagementApplication {
 	}
 
     /**
-     * H2 database를 별도로 설치 후 실행해야 합니다!!
-	 * 
-	 * H2 다운로드 후, 저장 경로로 이동
-	 * 
-     * Windows : ~\H2\bin 폴더 경로 이동 -> h2.bat 실행
-     * MacOS : ~/H2/bin 폴더 경로 이동 -> ./h2.sh 실행
-     *
-     * 처음 접속 시, 연결 후 전체 디렉토리에 test.mv.db 폴더가 생성되었는지 확인 + sql package 내 query 입력하여 테이블 생성
+	 * 처음 연결 시 :
 	 *
-     * 이후 jdbc:h2:tcp://localhost/~/test 경로 확인 후 접속 가능
+	 * H2 database 다운로드(https://www.h2database.com/html/main.html), 저장 경로로 이동
+	 * 
+     * Windows : 저장 경로\H2\bin -> h2.bat 실행
+     * Mac : 저장 경로/H2/bin -> h2.sh 실행
+     *
+     * 사용자 폴더에 test.mv.db 폴더가 생성되었는지 확인
+	 *
+     * H2 콘솔 JDBC URL : jdbc:h2:tcp://localhost/~/test 로 변경
+	 * application.properties : spring.datasource.url=jdbc:h2:tcp://localhost/~/test 로 변경
 	 */
 }
